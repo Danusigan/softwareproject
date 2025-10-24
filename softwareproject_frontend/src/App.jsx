a09d4967d31d04058e8fb4cc56aafc007411a083
thiruchelvamkopuraj@gmail.com <thiruchelvamkopuraj@gmail.com>
2025-10-25 01:28:58 +0530

    feat(home-page): add initial landing page with Tailwind CSS
    - Created React Vite app with Tailwind setup
    - Added LandingPage component under src/pages
    - Implemented hero section, features, tools, and footer
    -Adding header and footer components

---
+++ softwareproject_frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import LandingPage from './pages/landingpage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
