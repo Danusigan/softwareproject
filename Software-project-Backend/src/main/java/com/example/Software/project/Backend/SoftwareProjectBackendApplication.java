07dbf45b4af580f14c254e8f6405b2fdc6232a43
sanjil18 <sanjilraj2@gmail.com>
2025-10-24 09:58:21 +0530

    Creation of Backend

---
+++ Software-project-Backend/src/main/java/com/example/Software/project/Backend/SoftwareProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Software.project.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProjectBackendApplication.class, args);
+	}
+
+}
---
+++ Software-project-Backend/src/test/java/com/example/Software/project/Backend/SoftwareProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Software.project.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
