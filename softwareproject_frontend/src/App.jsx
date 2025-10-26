6243e3feb36149d6a2c89fa5ae9762f6c69cc0e7
Danusigan <j.danusigan02@gmail.com>
2025-10-26 13:19:33 +0530

    frontend

---
+++ softwareproject_frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import LandingPage from './pages/landingpage'
+import LoginPage from './pages/loginpage' 
+import ForgotPasswordPage from './pages/forgottenpasword'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/forgottenpassword" element={<ForgotPasswordPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
