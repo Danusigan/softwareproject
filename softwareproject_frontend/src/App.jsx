f2a5dabf5a1eb9092aeb25e5e8812a7d637600ce
Danusigan <j.danusigan02@gmail.com>
2025-10-29 10:42:55 +0530

    lo

--- softwareproject_frontend/src/App.jsx
+++ softwareproject_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import LandingPage from './pages/landingpage'
-import LoginPage from './pages/loginpage' 
+import LoginPage from './pages/loginpage'
 import ForgotPasswordPage from './pages/forgottenpasword'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
