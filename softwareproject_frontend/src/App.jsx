a8417012d8742564f709fd9593a457906860f9ec
Kopuraj <thiruchelvamkopuraj@gmail.com>
2026-01-21 01:08:04 +0530

    edite the ui
    create the super admin dashboard , and edit the lecturer page

--- softwareproject_frontend/src/App.jsx
+++ softwareproject_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/landingpage'
 import LoginPage from './pages/loginpage'
 import ForgotPasswordPage from './pages/forgottenpasword'
 import AdminDashboard from './pages/admindashboard'
+import SuperAdminDashboard from './pages/superadmindashboard'
 import LecturerDashboard from './pages/lecturerdashboard'
 import ModulesPage from './pages/modulespage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -18,6 +19,7 @@ function App() {
         <Route path="/loginpage" element={<LoginPage />} />
         <Route path="/forgottenpassword" element={<ForgotPasswordPage />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/super-admin-dashboard" element={<SuperAdminDashboard />} />
         <Route path="/lecturer-dashboard" element={<LecturerDashboard />} />
         <Route path="/modules" element={<ModulesPage />} />
       </Routes>
