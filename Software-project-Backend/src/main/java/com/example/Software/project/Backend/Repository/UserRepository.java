f340650b6ab0ffbfb3e5748d2aa4319d3e2b30f8
Kopuraj <thiruchelvamkopuraj@gmail.com>
2026-02-11 23:23:58 +0530

    update

--- Software-project-Backend/src/main/java/com/example/Software/project/Backend/Repository/UserRepository.java
+++ Software-project-Backend/src/main/java/com/example/Software/project/Backend/Repository/UserRepository.java
@@ -11,6 +11,7 @@ public class UserRepository {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import java.util.Optional;
 
 @Repository
@@ -26,6 +27,9 @@ public interface UserRepository extends JpaRepository<User, String> {
         return findByUsername(userID);
     }
 }
+=======
+}
+>>>>>>> Stashed changes
 =======
 }
 >>>>>>> Stashed changes
